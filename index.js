0d4ddf630bc8f440897c63b8f2d1b2b07ed94c57
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-25 13:55:26 +0100

    Reestructurar y añadir pruebas

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-let Shape = require('@alu0100821390/ull-shape');
-
-class Triangle extends Shape {
-    constructor(options) {
-      super(options)
-    }
-    area() {
-      return 0.5 * this.width * this.height;
-    }
-}
-Shape.Shapes.Triangle = Triangle;
-module.exports = Triangle;
+module.exports = require('./lib/triangle');
